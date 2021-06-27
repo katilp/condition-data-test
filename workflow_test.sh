0b548afa99c4a10b929b8251031069718690416f
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2021-06-27 18:15:11 +0200

    Create workflow_test.sh

---
+++ workflow_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+# parameters: $1 package, $2 branch, $3 configuration file with path from package root
+#             $4 GlobalTag $ GitHub organization/owner
+sudo chown $USER /mnt/vol
+pwd
+cd ~/CMSSW_5_3_32/src/
+source /opt/cms/cmsset_default.sh
+
+which git
+git --version
+
+ls /cvmfs/cms-opendata-conddb.cern.ch
