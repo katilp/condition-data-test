e98ffbec046ebd27421a578470882ba2b0d6fe08
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2021-06-30 11:36:34 +0200

    Update find_db.sh

--- find_db.sh
+++ find_db.sh
@@ -43,91 +43,64 @@ do
         exception=no
     else
         echo Exception is $exception
-        # check if the exception name is the db file name in the listing and count how many times
-        missingdbline="$(grep $exception original.txt | grep db)"
-        nmatches="$(echo $missingdbline | grep -o .db | wc -l)"
-        # check if the exception itself has the .db file extension
-        if [ "$(echo $exception | awk -F. '{print $(NF)}')" = db ]
-        then
-            missingdb="$(echo $exception | awk -F \/ '{print $(NF)}')"
-            echo Found $missingdb from the execption message
-        # check that only one match in the db listing with the db name corresponding to the exception  
-        elif [ $nmatches = 1 ]
-        then
-            missingdb="$(echo $missingdbline |  awk -F\' '{print $(NF-3)}' | awk -F\/ '{print $(NF)}' | grep db)"
-            echo Found $missingdb from the original db list 
-        elif [ $nmatches = 0 ]
-        then
-            echo No db line corresponding the exception message found
-            echo other matches in the db file:
-            grep $exception original.txt
-            missingdb=notfound
-            exception=no
-        else
-            echo Multiple db lines corresponding to the exception message:
-            echo $missingdbline
-            # need to handle it properly, for the moment stop the loop
-            echo Option 1:
-            filelist=`./dbname.py "$missingdbline"`
-            echo $filelist
-            echo Option 2:
-            filelist=$( ./dbname.py "$missingdbline" )
-            echo $filelist
-            
-            missingdb=notfound
-            exception=no
-        fi
+        missingdblines="$(grep $exception original.txt | grep db)"
+        filelist=$( ./dbname.py "$missingdblines" )
+        echo $filelist
+        dbnlist=$( ./dbnumber.py "$missingdblines" )
+        echo $dbnlist
         
-        echo Value of missingdb is $missingdb
+        names=( $filelist )
+        numbers=( $dbnlist )
         
-        # check if missing db has been found
-        if [ $missingdb != notfound ]
-        then
-        # copy the missing db file from cvmfs to the local directory
-        cp /cvmfs/cms-opendata-conddb.cern.ch/$globaltag/$missingdb $globaltag
+        for index in ${!names[*]}; do 
+            missingdb=${names[$index]}
+            dbnumber=${numbers[$index]}
+            missingdbline=$( ./dbline.py "$missingdblines" $index )
+            echo The db line is $missingdbline
+        
+            echo Value of missingdb is $missingdb
+            echo Value of dbnumber is $dbnumber
+        
+            # copy the missing db file from cvmfs to the local directory
+            cp /cvmfs/cms-opendata-conddb.cern.ch/$globaltag/$missingdb $globaltag
 
-        # find the db number
-        dbnumber="$(echo $missingdbline  | awk -F\, '{print $(NF-2)}')"
-        echo dbnumber $dbnumber
+            # find the name in the tag tree corresponding to this db number
+            tagtreename="$(grep \',$dbnumber, original.txt  | grep TAGTREE | awk -F\, '{print $(NF-4)}')"
 
-        # find the name in the tag tree corresponding to this db number
-        tagtreename="$(grep \',$dbnumber, original.txt  | grep TAGTREE | awk -F\, '{print $(NF-4)}')"
+            # get text file
+            sqlite3  $dbfile .dump > file_dump.txt
 
-        # get text file
-        sqlite3  $dbfile .dump > file_dump.txt
+            # add the missing line and substitute the db number with i
+            newdbline="$(echo "${missingdbline/$dbnumber,/$i,}")"
+            sed -i "/CREATE TABLE coral_sqlite_fk(id TEXT NOT NULL, name TEXT, tablename TEXT NOT NULL);/i $newdbline" file_dump.txt
+            echo Adding line $newdbline
 
-        # add the missing line and substitute the db number with i
-        newdbline="$(echo "${missingdbline/$dbnumber,/$i,}")"
-        sed -i "/CREATE TABLE coral_sqlite_fk(id TEXT NOT NULL, name TEXT, tablename TEXT NOT NULL);/i $newdbline" file_dump.txt
-        echo Adding line $newdbline
+            # change the two numbers for lines All and Calibration to 2*i+2 and 2*i+1
+            val1line="$(grep "VALUES(1," file_dump.txt)"
+            echo Modifying line $val1line
+            sed -i "s/$val1line/INSERT INTO TAGTREE_TABLE_"$globaltag" VALUES(1,4294967295,$((n+2)),'All',0,0,1,0);/g" file_dump.txt
+            val2line="$(grep "VALUES(2," file_dump.txt)"
+            sed -i "s/$val2line/INSERT INTO TAGTREE_TABLE_"$globaltag" VALUES(2,0,$((n+1)),'Calibration',0,1,2,0);/g" file_dump.txt
+            echo Modifying line $val2line
 
-        # change the two numbers for lines All and Calibration to 2*i+2 and 2*i+1
-        val1line="$(grep "VALUES(1," file_dump.txt)"
-        echo Modifying line $val1line
-        sed -i "s/$val1line/INSERT INTO TAGTREE_TABLE_"$globaltag" VALUES(1,4294967295,$((n+2)),'All',0,0,1,0);/g" file_dump.txt
-        val2line="$(grep "VALUES(2," file_dump.txt)"
-        sed -i "s/$val2line/INSERT INTO TAGTREE_TABLE_"$globaltag" VALUES(2,0,$((n+1)),'Calibration',0,1,2,0);/g" file_dump.txt
-        echo Modifying line $val2line
+            # get tag inventory total line and change it to 
+            taginventory="$(grep '"TAGINVENTORY_IDS" VALUES' file_dump.txt)"
+            sed -i "s/$taginventory/INSERT INTO \"TAGINVENTORY_IDS\" VALUES($i);/g" file_dump.txt
+            echo Modifying line $taginventory
 
-        # get tag inventory total line and change it to 
-        taginventory="$(grep '"TAGINVENTORY_IDS" VALUES' file_dump.txt)"
-        sed -i "s/$taginventory/INSERT INTO \"TAGINVENTORY_IDS\" VALUES($i);/g" file_dump.txt
-        echo Modifying line $taginventory
+            # add tagtree line
+            newdbfileline="INSERT INTO "TAGTREE_TABLE_"$globaltag"" VALUES($n,0,$((n+1)),$tagtreename,$i,2,$((i+2)),0);"
+            sed -i "/CREATE TABLE \"TAGTREE_"$globaltag"/i $newdbfileline" file_dump.txt
+            echo Adding line $newdbfileline
 
+            # get total values 
+            tagtree="$(grep 'INSERT INTO "TAGTREE_'$globaltag'' file_dump.txt)"
+            sed -i "s/$tagtree/INSERT INTO TAGTREE_"$globaltag"_IDS VALUES($((i+2)));/g" file_dump.txt
+            echo Modifying line 'INSERT INTO TAGTREE_'$globaltag'_IDS VALUES('$((i+2))');'
 
-        # add tagtree line
-        newdbfileline="INSERT INTO "TAGTREE_TABLE_"$globaltag"" VALUES($n,0,$((n+1)),$tagtreename,$i,2,$((i+2)),0);"
-        sed -i "/CREATE TABLE \"TAGTREE_"$globaltag"/i $newdbfileline" file_dump.txt
-        echo Adding line $newdbfileline
-
-        # get total values 
-        tagtree="$(grep 'INSERT INTO "TAGTREE_'$globaltag'' file_dump.txt)"
-        sed -i "s/$tagtree/INSERT INTO TAGTREE_"$globaltag"_IDS VALUES($((i+2)));/g" file_dump.txt
-        echo Modifying line 'INSERT INTO TAGTREE_'$globaltag'_IDS VALUES('$((i+2))');'
-
-        rm $dbfile
-        cat file_dump.txt | sqlite3 $dbfile
-        fi
+            rm $dbfile
+            cat file_dump.txt | sqlite3 $dbfile
+        done
     fi
 
 done    
