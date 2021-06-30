4161ad302d9227f9ff7ddf0566998d8e6240d08c
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2021-06-30 16:38:27 +0200

    Update find_db.sh

--- find_db.sh
+++ find_db.sh
@@ -101,10 +101,10 @@ do
             sed -i "s/$tagtree/INSERT INTO TAGTREE_"$globaltag"_IDS VALUES($((i+2)));/g" file_dump.txt
             echo Modifying line 'INSERT INTO TAGTREE_'$globaltag'_IDS VALUES('$((i+2))');'
 
-            echo The updated db file is:
-            cat file_dump.txt
-            echo These db files have been copied:
-            ls $globaltag
+            #echo The updated db file is:
+            #cat file_dump.txt
+            #echo These db files have been copied:
+            #ls $globaltag
 
             rm $dbfile
             cat file_dump.txt | sqlite3 $dbfile
@@ -116,6 +116,7 @@ done
 echo These db files have been copied:
 ls $globaltag
 
+ls -l /mnt/vol
 cp -r $globaltag /mnt/vol/products
 
 echo The main db file is:
